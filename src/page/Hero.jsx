45291efd4281c801f569cd67159b45014b91949f
zharsond <muhammad.sondjaja@satu.ac.id>
2026-05-06 17:04:53 +0700

    typo fix

--- src/page/Hero.jsx
+++ src/page/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     <div className="relative w-full h-screen">
       <img src="/HeroBackground.png" alt="pawpewpew" className="object-cover absolute inset-0 -z-5" />
       <div className="w-full h-screen flex flex-col justify-center items-center gap-10">
-        <img src="/Logo.png" alt="pawpewpew" className="w-120" />
+        <img src="/logo.png" alt="pawpewpew" className="w-120" />
         <div className="flex gap-2 justify-center">
           <CallToAction image={`/steam.png`}>WISHLIST ON STEAM</CallToAction>
           <CallToAction image={`/discord.png`}>JOIN OUR COMMUNITY</CallToAction>

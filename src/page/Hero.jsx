437bdb9bd6c3c6abea52df47e0db83c0772874de
zharsond <muhammad.sondjaja@satu.ac.id>
2026-05-06 17:02:14 +0700

    /public delete

--- src/page/Hero.jsx
+++ src/page/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
       <div className="w-full h-screen flex flex-col justify-center items-center gap-10">
         <img src="/Logo.png" alt="pawpewpew" className="w-120" />
         <div className="flex gap-2 justify-center">
-          <CallToAction image={`/public/steam.png`}>WISHLIST ON STEAM</CallToAction>
-          <CallToAction image={`/public/discord.png`}>JOIN OUR COMMUNITY</CallToAction>
+          <CallToAction image={`/steam.png`}>WISHLIST ON STEAM</CallToAction>
+          <CallToAction image={`/discord.png`}>JOIN OUR COMMUNITY</CallToAction>
         </div>
       </div>
     </div>

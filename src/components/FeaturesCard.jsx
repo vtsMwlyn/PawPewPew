141031f8dc5ed0f24c19db1111d1da4368210b5f
zharsond <muhammad.sondjaja@satu.ac.id>
2026-05-20 16:42:00 +0700

    build, hehehehe

--- src/components/FeaturesCard.jsx
+++ src/components/FeaturesCard.jsx
@@ -1,7 +1,7 @@
 export default function FeaturesCard({ title, subtitle, description, image }) {
   return (
     <div className="flex flex-col text-white text-shadow-[-2px_3px_0px_#0F1B24] items-center 
-    py-5 2xl:py-15 
+    py-5 2xl:py-10 
     px-5 2xl:px-15 
     w-full h-130 2xl:h-160 
     bg-hijaubaru-reverse border-blueblack border-5 rounded-3xl 
@@ -9,7 +9,7 @@ export default function FeaturesCard({ title, subtitle, description, image }) {
     drop-shadow-[-6px_7px_6px_#0F1B24]">
       <img src={image} alt={title} 
         className="border-blueblack border-5 rounded-3xl 
-        h-70 object-cover"
+        h-65 object-cover"
         />
       <div>
         <h1 className="text-xl text-center">{title}</h1>

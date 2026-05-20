5463468072921e9fdb5bc651dcc727caaf8601ba
zharsond <muhammad.sondjaja@satu.ac.id>
2026-05-20 16:27:19 +0700

    Features Card Height Fixed

--- src/components/FeaturesCard.jsx
+++ src/components/FeaturesCard.jsx
@@ -3,7 +3,7 @@ export default function FeaturesCard({ title, subtitle, description, image }) {
     <div className="flex flex-col text-white text-shadow-[-2px_3px_0px_#0F1B24] items-center 
     py-5 2xl:py-15 
     px-5 2xl:px-15 
-    w-full h-130 2xl:h-180 
+    w-full h-130 2xl:h-160 
     bg-hijaubaru-reverse border-blueblack border-5 rounded-3xl 
     gap-3 2xl:gap-6 
     drop-shadow-[-6px_7px_6px_#0F1B24]">

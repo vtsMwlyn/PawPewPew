74a4ba01c916327030d668523afda1f163d611ba
VTS <vannestheo@gmail.com>
2026-05-22 11:42:04 +0700

    custom video play button

--- src/page/WorldMeadow.jsx
+++ src/page/WorldMeadow.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import { useRef } from "react"
+
 export default function WorldMeadow() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  const handlePlayButtonClick = () => {
+    if (!isPlaying) {
+      videoRef.current.play();
+      setIsPlaying(true);
+    }
+    else {
+      videoRef.current.pause();
+      setIsPlaying(false);
+    }
+    
+  }
+
   return (
     <section className="w-full">
       {/* A peaceful sanctuary for all. */}
@@ -22,10 +40,14 @@ export default function WorldMeadow() {
       <div className="w-full flex flex-col bg-hijaubaru-light items-center justify-center py-10 gap-6 px-8 lg:p-0">
         <h1 className="text-2xl lg:text-4xl text-white uppercase text-shadow-[-2px_6px_0px_#0F1B24] 
         w-full lg:w-4xl text-center">Life in the village of Meadow was peaceful, whimsical, and perfectly happy </h1>
-        <video className="w-full max-w-7xl rounded-2xl lg:rounded-4xl border-6 
-          border-bs-blueblack shadow-[-10px_10px_25px_#000000]" controls>
-          <source src="/video.mp4" type="video/mp4"/>
-        </video>
+        <div className="relative w-full max-w-7xl h-165 rounded-2xl overflow-hidden bg-black">
+          <video ref={videoRef} className="w-full h-full lg:rounded-4xl border-6 border-bs-blueblack shadow-[-10px_10px_25px_#000000] object-fit">
+            <source src="/sample.mp4" type="video/mp4" />
+          </video>
+          <button type="button" onClick={handlePlayButtonClick} className={`w-full h-full flex justify-center items-center bg-[rgba(0,0,0,0.8)] absolute top-0 transition ease-in-out duration-200 ${isPlaying && 'opacity-0'}`}>
+            <img src="/play-button.webp" />
+          </button>
+        </div>
         <p className="text-white comic-relief-bold text-lg lg:text-2xl w-full lg:w-4xl text-center text-shadow-[-2px_4px_0px_#0F1B24]">
           But when a relentless Robo-Beast army crashes in and kidnaps the villagers, playtime is officially over.
         </p>
